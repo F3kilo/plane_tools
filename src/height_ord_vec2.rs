598c70ba6339f6a6692594f8e1cc63414ccce28d
f3kilo <f3kilo@yandex.ru>
2020-02-21 12:19:10 +0300

    height ordered Vec2

---
+++ src/height_ord_vec2.rs
@@ -0,0 +1,32 @@
+use glam::Vec2;
+use std::cmp::Ordering;
+use crate::vec2cmp;
+
+pub struct HeightOrdVec2(pub Vec2);
+
+// Newtype pattern for glam::Vec2 to allow compare two vectors by height.
+impl From<Vec2> for HeightOrdVec2 {
+    fn from(v: Vec2) -> Self {
+        Self(v)
+    }
+}
+
+impl PartialEq for HeightOrdVec2 {
+    fn eq(&self, r: &Self) -> bool {
+        vec2cmp::cmp_y(self.0, r.0) == Ordering::Equal
+    }
+}
+
+impl PartialOrd for HeightOrdVec2 {
+    fn partial_cmp(&self, r: &Self) -> Option<Ordering> {
+        Some(vec2cmp::cmp_y(self.0, r.0))
+    }
+}
+
+impl Eq for HeightOrdVec2 {}
+
+impl Ord for HeightOrdVec2 {
+    fn cmp(&self, r: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(r).unwrap()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod vec2cmp;
 pub mod order;
 pub mod intersect;
 pub mod graph;
+pub mod height_ord_vec2;
